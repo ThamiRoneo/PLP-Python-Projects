a20614a769385e475e8fe6247ee4b7ce4ddb3026
ThamiRoneo <thamironeo.sithole@icloud.com>
2024-11-13 17:12:36 +0200

    updated calculator.py

--- week-1-python-assignment/calculator.py
+++ week-1-python-assignment/calculator.py
@@ -1,9 +1,10 @@
 # declaration of variables that will assign user inputs
-num1 = input("Enter an integer of your choosing: ")
-num2 = input("Enter another integer of your choosing: ")
+num1 = input("Enter a number of your choosing: ")
+num2 = input("Enter another number of your choosing: ")
 operator = input("Enter a mathematical operation (+, -, /, *): ")
 
-#
+# converting user inputs to integers
 results = eval(num1 + operator + num2)
 
+# printing the result of the operation
 print("The results are: ", num1, operator, num2, " = ", results)
